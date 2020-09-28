ab4e93080d4f019b67c582d4ce075d98ebc34894
a1q123456 <a1q123456@users.noreply.github.com>
2020-09-28 14:46:16 +0800

    cabac tables

--- H264Decoder/Decoding/CabacDecoder.h
+++ H264Decoder/Decoding/CabacDecoder.h
@@ -8,15 +8,43 @@ struct PrefixAndSuffix { };
 
 struct UEG3 {};
 
+class ArithmeticDecodingEngine
+{
+public:
+    ArithmeticDecodingEngine(BitstreamReader& reader)
+    {
+        codIOffset = reader.readBits<int, 9>();
+    }
+
+private:
+    int codIRange = 510;
+    int codIOffset;
+};
+
 class CabacDecoder
 {
 public:
     CabacDecoder(int picInitQpMinus26, int sliceQpDelta, int cabacInitIdc, SliceType type);
 
+    void ResetAde()
+    {
+        ade.reset();
+    }
+
+    template<typename ...BinarizationMethods>
+    void Binarization()
+    {
+
+    }
+
     template<typename ...BinarizationMethods>
     int Decode(BitstreamReader& reader, AeV<BinarizationMethods...>& field)
     {
-
+        if (!ade)
+        {
+            ade.reset(ArithmeticDecodingEngine{ reader });
+        }
+        Binarization<BinarizationMethods...>()
     }
 private:
 
@@ -29,5 +57,7 @@ private:
     };
 
     std::unordered_map<int, CabacContext> contexts;
+    
+    std::unique_ptr<ArithmeticDecodingEngine> ade;
 };
 
